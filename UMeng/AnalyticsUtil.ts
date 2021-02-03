d2e1c9c1cb4368deca4fd941387b8a27cbec7816
Damoness <qjwdcool@qq.com>
2021-02-03 10:52:56 +0800

    feat: 新增AnalyticsUtil 类 1. onPageBegin 2. onPageEnd

--- UMeng/AnalyticsUtil.ts
+++ UMeng/AnalyticsUtil.ts
@@ -1,5 +1,16 @@
 /**
  * Created by Damoness on 19/8/30.
  */
-var {NativeModules} = require('react-native');
-module.exports = NativeModules.UMAnalyticsModule;
+
+var { NativeModules } = require("react-native");
+
+const AnalyticsModule = NativeModules.UMAnalyticsModule;
+
+export default class AnalyticsUtil {
+  static onPageBegin(pageName: string) {
+    AnalyticsModule.onPageBegin(pageName);
+  }
+  static onPageEnd(pageName: string) {
+    AnalyticsModule.onPageEnd(pageName);
+  }
+}
